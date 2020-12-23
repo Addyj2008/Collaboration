cc6a60b7f1bcf49de43de59ef77ede1876551a49
Aadish <addyj2908@gmail.com>
2020-12-23 19:16:28 +0530

    Added back button

--- JS/levelScreen.js
+++ JS/levelScreen.js
@@ -1,13 +1,12 @@
 const LEVEL = [];
+let currentLevel;
 for (let loop1 in document.getElementsByClassName("level")) {
     if (isFinite(loop1 * 1)) {
         LEVEL.push(document.getElementsByClassName("level")[loop1]);
     }
 }
-function displayLevelScreen () {
-    for (let loop1 in LEVEL) {
-        document.getElementById("level-container").appendChild(LEVEL[loop1]);
-    }
+function displayLevelScreen (level) {
+    document.getElementById("level-container").appendChild(LEVEL[level]);
 }
 function hideLevelScreen () {
     for (let loop1 in LEVEL) {
--- JS/selectionScreen.js
+++ JS/selectionScreen.js
@@ -20,12 +20,20 @@ window.requestAnimationFrame(function () {
     for (loop1 in LS) {
         LS[loop1].style['left'] = window.innerWidth/4 * (loop2 % 4) + window.innerWidth/40 + "px";
         LS[loop1].style['top'] = window.innerHeight/4 * (loop2 - (loop2 % 4))/4 + window.innerHeight/40 + "px";
-        LS[loop1].onclick = function () {
-            if (this.src.slice(this.src.length - 13) !== "levelLock.png") {
-                document.getElementById("level-container").appendChild(LEVEL[this.id.slice(5, LS[1].id.length - 7) - 1]);
-                hideSelectionScreen();
+        if (LS[loop1].nodeName === "IMG") {
+            LS[loop1].onclick = function () {
+                if (this.src.slice(this.src.length - 13) !== "levelLock.png") {
+                    displayLevelScreen(this.id.slice(5, LS[1].id.length - 7) - 1);
+                    hideSelectionScreen();
+                }
             }
         }
         loop2++;
     }
 });
+
+let back = document.getElementById('back');
+back.onclick = function () {
+    hideSelectionScreen();
+    displayHomeScreen();
+}
